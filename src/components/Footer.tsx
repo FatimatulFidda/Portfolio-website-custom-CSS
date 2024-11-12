37d018d60c1c2f7cf9193ca4373a63f272c564ae
Fatima tul fidda <fatimatulfidda73ansari@gmail.com>
2024-11-12 07:13:00 -0800

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../app/Styles/footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Fatima tul fidda | rights reserved 2024.</p>
+    </div>
+  );
+};
+
+export default Footer;
